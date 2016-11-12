3608bb8f9914f92628a5a3f10fb6161fdd11ef40
Leonardo34 <leo_broch@hotmail.com>
2016-11-12 11:41:20 -0200

    Iniciados métodos contaFrequencia e analyser da Classe ContaFrequecia

--- src/ContaFrequencia.java
+++ src/ContaFrequencia.java
@@ -1,7 +1,23 @@
-import com.sun.javafx.collections.MappingChange.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
 
 public class ContaFrequencia {
-	public static void contaFrequencia () {
-		//TODO
+	public static Map<String, Integer> contaFrequencia (String diretorio) throws Exception {
+		try {
+			List<String> linhas = Files.readAllLines(Paths.get(diretorio));
+		}
+		catch (IOException e) {
+			System.out.println("Erro");
+			throw new Exception (e.getMessage());
+		}
+		return null;
+	}
+	
+	private static Map<String, Integer> analyser (List<String> linhas) {
+		return null;
 	}
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,8 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
-public class MainClass {
+public class MainClass{
 	public static void main (String[] args) {
 		System.out.println("Contador de Frequência de palavras em texto");
 		System.out.println("Autor: Leonardo Broch de Morais");
@@ -8,5 +10,11 @@ public class MainClass {
 		System.out.println("Digite o diretório do arquivo: ");
 		String diretorio = new Scanner (System.in).next();
 		
+		if (Files.exists(Paths.get(diretorio))) {
+			
+		}
+		else {
+			System.out.println("Diretorio Incorreto");
+		}
 	}
 }
