3cd370ddfb09b4f88d6da7abfa48678a87c50225
Leonardo34 <leo_broch@hotmail.com>
2016-11-12 11:20:39 -0200

    Iniciado Menu da aplicação

--- src/ContaFrequencia.java
+++ src/ContaFrequencia.java
@@ -1,4 +1,7 @@
+import com.sun.javafx.collections.MappingChange.Map;
 
 public class ContaFrequencia {
-
+	public static void contaFrequencia () {
+		//TODO
+	}
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 
 public class MainClass {
-
+	public static void main (String[] args) {
+		System.out.println("Contador de Frequência de palavras em texto");
+		System.out.println("Autor: Leonardo Broch de Morais");
+		System.out.println("Contato: leo_broch@hotmail.com");
+		System.out.println("Digite o diretório do arquivo: ");
+		String diretorio = new Scanner (System.in).next();
+		
+	}
 }
